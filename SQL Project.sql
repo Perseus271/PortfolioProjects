2b9f36d55a4261fa2ee58b8ef5f753b9c4df3bd5
Perseus271 <96358163+Perseus271@users.noreply.github.com>
2023-08-27 22:41:57 -0500

    Update SQL Project.sql

--- SQL Project.sql
+++ SQL Project.sql
@@ -10,7 +10,7 @@ Order by 1,2
 -- Same query with "international" location
 
 --Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
---From PortfolioProject..CovidDeaths
+--From PortfolioProject..covid_deaths$
 ----Where location like '%states%'
 --where location = 'World'
 ----Group By date
@@ -18,9 +18,9 @@ Order by 1,2
 
 -- 2
 
---removing outliers
+--removing outliers to match above query
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
-From PortfolioProject..CovidDeaths
+From PortfolioProject..covid_deaths$
 --Where location like '%states%'
 Where continent is null 
 and location not in ('World', 'European Union', 'International', 'High income', 'Upper middle income', 'Lower middle income', 'Low income')
@@ -30,7 +30,7 @@ order by TotalDeathCount desc
 -- 3
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
+From PortfolioProject..covid_deaths$
 --Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc
@@ -40,7 +40,7 @@ order by PercentPopulationInfected desc
 
 
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
+From PortfolioProject..covid_deaths$
 --Where location like '%states%'
 Group by Location, Population, date
 order by PercentPopulationInfected desc
